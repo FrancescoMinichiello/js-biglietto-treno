f5efdd36c4cb3bdaf864830f061807eaea3bac18
FrancescoMinichiello <fraps2278@gmail.com>
2024-07-31 16:17:58 +0200

    add css, image and calc js

--- script.js
+++ script.js
@@ -7,15 +7,30 @@
 */
 
 // # Fase preparatoria
+const priceElement = document.getElementById('price')
 
-const price = 0.21;
-console.log(price);
+const kmPrice = 0.21;
+console.log('kmPrice', kmPrice);
 
 
 // # Fase raccolta dati
-const kilometres = prompt('Quanti chilometri vuoi percorrere?', '10')
-console.log(kilometres);
-const age = prompt('Quanti anni hai?', '17')
-console.log(age);
+const kilometres = parseInt(prompt('Quanti chilometri vuoi percorrere?', 10))
+console.log('kilometres', kilometres);
+const age = parseInt(prompt('Quanti anni hai?', 17))
+console.log('age', age);
+
+// # Fase calcolo prezzo biglietto
+
+let total = kmPrice * kilometres
+console.log('total', total);
 
 // # Fase calcolo sconto
+
+if (age >= 65) {
+    total *= 0.6;
+} else if (age < 18) {
+    total *= 0.8;
+}
+
+total = total.toFixed(2) + '€'
+priceElement.innerHTML = total;
